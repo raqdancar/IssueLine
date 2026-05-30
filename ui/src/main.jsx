65263624d9b507f57ec57a8c1e67664c15437191
Daniel Larrosa <larrosa.dan@gmail.com>
2026-05-30 12:17:54 +0200

    feat: add Vercel Speed Insights integration to the application

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
       <I18nProvider>
         <BrowserRouter>
           <App />
+          <SpeedInsights />
         </BrowserRouter>
       </I18nProvider>
     </QueryClientProvider>
